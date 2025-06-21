5cedd803aa4430c264bbd4d971734ad0f530ddf1
Yojitkataria <72148843+Yojitkataria@users.noreply.github.com>
2025-06-21 15:13:06 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = https://webrtc-backend-u9y7.onrender.com/api || 'http://localhost:5000/api';
 
 class ApiService {
   constructor() {
